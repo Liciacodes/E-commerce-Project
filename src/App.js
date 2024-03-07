661f5320a92fbb218492c0e711778b3231234c43
Liciacodes <udosenfelicia@gmail.com>
2024-03-07 14:35:35 +0100

    refactored the Fetch Api call and dealt with errors that occured during the fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Category from "./components/Category";
+import { getCategories, getProducts } from "./fetcher";
 
 function App() {
-  const [results, setResults] = useState([]);
+  const [categories, setCategories] = useState({ errorMessage: "", data: [] });
+  const [products, setProducts] = useState({ errorMessage: "", data: [] });
 
   useEffect(() => {
-    fetch("http://localhost:3001/categories")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setResults(data);
-      });
+    const fetchData = async () => {
+      const responseObject = await getCategories();
+
+      setCategories(responseObject);
+    };
+    fetchData();
   }, []);
 
+  const handleCategoryClick = (id) => {
+    const fetchData = async () => {
+      const responseObject = await getProducts(id);
+      setProducts(responseObject);
+    };
+    fetchData();
+  };
+
+  const renderCategories = () => {
+    return categories.data.map((c) => (
+      <Category
+        key={c.id}
+        id={c.id}
+        title={c.title}
+        onCategoryClick={() => handleCategoryClick(c.id)}
+      />
+    ));
+  };
+
+  const renderProducts = () => {
+    return products.data.map((p) => <div>{p.title}</div>);
+  };
+
   return (
-    <div className="App">
-      {results.map((d) => (
-        <div key={d.id}>{d.title}</div>
-      ))}
-    </div>
+    <>
+      <header>My Store</header>
+      <section>
+        <nav>
+          {categories.errorMessage && (
+            <div> Error:{categories.errorMessage}</div>
+          )}
+          {categories.data && renderCategories()}
+        </nav>
+        <article>
+          <h1>Products</h1>
+          {products && renderProducts()}
+          {products.errorMessage && <div>{products.errorMessage}</div>}
+        </article>
+      </section>
+
+      <footer>footer</footer>
+    </>
   );
 }
 
